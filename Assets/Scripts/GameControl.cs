6d26fc67de91dcbd04bf0a616a3c1b61ba290c41
yhharryc <yh.harryc@hotmail.com>
2025-03-13 23:58:07 -0600

    deleted code for building

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -6,7 +6,7 @@ public class GameControl : MonoBehaviour
 {
     public GameObject Player;
     public BlackOutUI blackOutUI;
-    public EscapeBtn EscapeBtn;
+    //public EscapeBtn EscapeBtn;
 
     private static GameControl _gameControl = null;
     public static GameControl Game
@@ -28,7 +28,7 @@ public class GameControl : MonoBehaviour
         Player = GameObject.FindGameObjectWithTag("Player");
         blackOutUI= GameObject.Find("BlackOut").GetComponent<BlackOutUI>();
         blackOutUI.SetTransparent(0);
-        EscapeBtn= GameObject.Find("EscapeBtn").GetComponent<EscapeBtn>();
+        //EscapeBtn= GameObject.Find("EscapeBtn").GetComponent<EscapeBtn>();
     }
 
 
